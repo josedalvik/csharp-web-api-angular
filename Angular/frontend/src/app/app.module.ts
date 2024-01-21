8248095c1d395f357ab1674563d80a7f89cb8f93
josedalvik <josedalvik@gmail.com>
2024-01-21 16:09:53 -0600

    Personalización de plantilla de frontend

--- Angular/frontend/src/app/app.module.ts
+++ Angular/frontend/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      [
+        {path:'', component: HomeComponent}
+      ]
+    ), 
   ],
   providers: [],
   bootstrap: [AppComponent]
